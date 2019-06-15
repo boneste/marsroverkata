7c99987cc5aa51fc9c1c346581325b1d84e5a214
Stefano Bonelli <bone@Stefanos-MacBook-Pro.local>
2019-06-15 21:05:38 +0200

    Added simple console interaction interface

--- src/MarsRoverKata.Application/Program.cs
+++ src/MarsRoverKata.Application/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using MarsRoverKata.Domain;
 
 namespace MarsRoverKata.Application
 {
@@ -6,7 +8,31 @@ namespace MarsRoverKata.Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Coming soon...");
+            var obstacleMap = new List<Coordinates>()
+            {
+                Coordinates.Create(3,5),
+                Coordinates.Create(6,4)
+            };
+
+            var obstacleDetector = ObstacleDetector.Create(obstacleMap);
+
+            var rover = Rover.Create(1, 1, "N", obstacleDetector);
+            Console.WriteLine($"Welcome to Mars, you landed in x: {rover.Position.X} y: {rover.Position.Y} and your direction is {rover.Direction.Code}");
+            Console.WriteLine("Please insert commands:");
+
+            while (true)
+            {
+                var commandString = Console.ReadLine();
+
+                if (commandString == "Quit")
+                    return;
+
+                var result = rover.ExecuteCommandSequence(commandString.ToCharArray());
+
+                Console.WriteLine(result.Result.ToString());
+                Console.WriteLine($"Your actual position is x: {rover.Position.X} y: {rover.Position.Y} and your direction is {rover.Direction.Code}");
+                Console.WriteLine(result.Message);
+            }
         }
     }
 }
